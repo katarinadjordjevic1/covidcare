cf4d1ea5a92c071d01841d547925323044cdea15
katarinadjordjevic1 <kaca95bgd@gmail.com>
2020-05-05 14:26:15 +0200

    mysql connector, migratus, karma, hiccup and buddy deps added

--- project.clj
+++ project.clj
@@ -4,7 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [mysql/mysql-connector-java "8.0.20"]
+                 [migratus "1.2.8"]
+                 [korma "0.4.3"]                 
+                 [hiccup "1.0.5"]
+                 [buddy/buddy-auth "2.2.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler covidcare.handler/app}
   :profiles
