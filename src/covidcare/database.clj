4d577a11391bd9255d3a56c2ad82217dc6f59a5b
katarinadjordjevic1 <kaca95bgd@gmail.com>
2020-05-05 19:57:45 +0200

    database handler created

---
+++ src/covidcare/database.clj
@@ -0,0 +1,21 @@
+(ns covidcare.database
+  (:require [clojure.java.jdbc :as sql]
+            [korma.core :as k]
+            [korma.db :refer [defdb mysql]])
+  (:import java.sql.DriverManager))
+
+(def db-config(clojure.edn/read-string (slurp "database/migratus-conf.edn")))
+
+(defdb db (mysql db-config))
+
+(k/defentity user
+             (k/table :user))
+
+(k/defentity schedule
+             (k/table :schedule))
+
+(defn get-all-users[]
+  (k/select user))
+
+(defn get-all-schedules[]
+  (k/select schedule))
