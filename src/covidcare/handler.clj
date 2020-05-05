fe4dae15da8a2ca265ffd9c1c576fa2efd7c2371
katarinadjordjevic1 <kaca95bgd@gmail.com>
2020-05-05 20:02:23 +0200

    buddy authentication added

--- src/covidcare/handler.clj
+++ src/covidcare/handler.clj
@@ -1,11 +1,33 @@
 (ns covidcare.handler
   (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [compojure.route :as route]            
+            [buddy.auth.backends.session :refer [session-backend]]
+            [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+  (:use [clojure.pprint]))
+
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
+
+(defn req-res-displayer [handler]
+  (fn [req]
+    (let [res (handler req)]
+      (println "\nRequest:")
+      (clojure.pprint/pprint req)
+      (println "\nResponse:")
+      (clojure.pprint/pprint res)
+      res)))
+
+
+(def backend (session-backend))
+
+
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (req-res-displayer)
+      (wrap-authentication backend)
+      (wrap-authorization backend)
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))))
