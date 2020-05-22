33926b52fb2d94775469d5a261c46708d3b7278e
katarinadjordjevic1 <kaca95bgd@gmail.com>
2020-05-22 16:54:27 +0200

    email, image bufgixes

--- src/covidcare/rootroute.clj
+++ src/covidcare/rootroute.clj
@@ -16,7 +16,7 @@
 (defn login-submit [{{user :username pass :password :as params} :params session :session}]
   (if (or (nil? user) (nil? pass))
     (v/login "No username or password")
-    (let [dbparams (if (nil? (validate-email user)) {:username user :password pass} {:email user :password pass})
+    (let [dbparams (if (nil? (valid-email? user)) {:username user :password pass} {:email user :password pass})
           userdata (first (db/get-user-by-params dbparams))]
       (if (empty? userdata)
         (v/login "Invalid username or password")
